f07f3ce776b584fc1b5390ef6cdc26f2e98af718
Harlan <hsavinturner@gmail.com>
2022-04-09 21:54:49 -0400

    stack size function, list to stack function

--- fsexercises/Program.fsx
+++ fsexercises/Program.fsx
@@ -30,3 +30,57 @@ let t3 = Plus(Num(1),Num(2))
 printfn "%s" (tostring t1);; // prints (5- -2) now, but should print (5 + 2)
 printfn "%s" (tostring t2);; // prints (1 + -1) now, but should print (1 - 1)
 printfn "%s" (tostring t3);;
+
+
+
+//EXERCISE C2: function to return the length of a stack
+
+type 'a stack = Emp | Apnd of 'a stack * 'a;;
+let s = Apnd(Apnd(Apnd(Emp,2),3),5);;
+
+let pushon a stk = Apnd(stk,a);;
+let popoff = function
+  | Emp -> raise (System.Exception("stack underflow"))
+  | Apnd(s,a) -> a;;
+
+//tail recursive tolist
+//2 curried functions --> first is ax, second is passed to the pattern matching clauses
+let rec tolist2 ax = function
+  | Emp -> ax    // ax is the accumulator, which starts as an empty list
+  | Apnd(s,a) -> tolist2 (a::ax) s;;
+
+
+
+//need to form a closure somehow out of this
+let size (*(M:'a stack)*) M = 
+  
+
+  //don't like having to call size with an extra initial parameter to intialize the accumulator, using helper to hide it
+  let rec inner M x =
+    let mutable iax = x
+    match M with
+    | Emp -> iax
+    | Apnd(s,a) -> iax<-iax+1; inner s iax //increase accumulator, pop top, call on remaining stack
+  
+  let result = inner M 0
+  result;;
+
+
+
+size s;;
+
+
+
+//EXERCISE C3: convert list into stack without reversing the elements
+let rec tostack (ax, (M: List<'T>))  = 
+  match M with 
+  | []  -> ax   // ax is an empty stack
+  | head :: tail -> tostack (Apnd(ax,head), tail) ;;
+
+
+
+let l1 = ["123","456"];;
+let l2 = [50;100;150];;
+
+let s1 = tostack(Emp,l2);;
+
